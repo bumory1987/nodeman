2106d73ef51e9229d47178616a31c3ce3c7aa5fd
park sungbum <parksungbum@parks-Mac-Studio.local>
2022-06-05 06:47:09 +0900

    prepare

---
+++ src/main.js
@@ -0,0 +1,4 @@
+// @ts-check
+// Formatting, Linting
+// Formatting: Prettier
+// Linting: ESLint
---
+++ src/test.js
@@ -0,0 +1,13 @@
+// @ts-check
+// Formatting, Linting
+
+/* eslint-disable-next-line no-console */
+console.log("hello world!")
+
+const x = 1
+/* eslint-disable-next-line no-console */
+console.log(x)
+
+const someString = "hell"
+const result = Math.log(someString)
+console.log(result)
